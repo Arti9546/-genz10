65b302e0c61ca264457eb3fecd3cb6f463b7f893
Arti9546 <artikumari54833@gmail.com>
2025-04-01 12:00:23 +0530

    IPC_app.py

--- IPC_app.py
+++ IPC_app.py
@@ -10,7 +10,7 @@ from langchain.text_splitter import RecursiveCharacterTextSplitter
 from sentence_transformers import SentenceTransformer
 
 # Initialize Groq LLM
-GROQ_API_KEY = st.secrets["beastywhite"]
+GROQ_API_KEY = st.secrets[""]
 llm = ChatGroq(model_name="llama-3.3-70b-versatile", api_key=GROQ_API_KEY)
 
 # Load Hugging Face embedding model
